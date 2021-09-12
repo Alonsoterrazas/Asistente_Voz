667d5db2e383b614db0a486601eedf95c3431421
Ivan Sanchez <ivanhumberto_16@hotmail.com>
2021-09-11 23:51:17 -0600

    Modelo de spotify terminado

--- Models/Spotify/playback.py
+++ Models/Spotify/playback.py
@@ -1,43 +1,36 @@
 import spotipy
-import requests
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.exceptions import SpotifyException
 from decouple import config
-import os
 import time
+import re
+import requests
 
 CLIENT_ID = config('SPOTIPY_CLIENT_ID')
 CLIENT_SECRET = config('SPOTIPY_CLIENT_SECRET')
 REDIRECT_URL = config('SPOTIPY_REDIRECT_URI')
 
-scopes = 'user-read-playback-state,user-modify-playback-state,user-read-currently-playing'
+scopes = 'user-read-playback-state,user-modify-playback-state,user-read-currently-playing,playlist-modify-private,' \
+         'playlist-read-private,playlist-modify-public'
 
 sa = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URL,
                   scope=scopes)
 sp = spotipy.Spotify(auth_manager=sa)
 states = ['context', 'track', 'off']
-'''
--Reanudar el playback
--añadir a la cola
--pausar el playback
--Pasar siguiente cancion
--regresar cancion
--reproducir una cancion
--cambiar de dispositivo
--activar/desactivar shuffle
--activar/desactivar repeat
-'''
+regexsuri = r'\b(?:spotify:track:)[A-Za-z0-9]+'
 
 
-def reproducir_cancion(cancion, device='COMPUTADORA'):
+def reproducir_cancion(cancion):
     try:
         track = sp.search(q=f'track:{cancion}', type='track')
         time.sleep(.2)
         track = track['tracks']['items'][0]['uri']
+
         cp = sp.currently_playing()
         cp = cp['item']['uri']
         if track == cp:
             return
+
         sp.add_to_queue(track)
         cola = []
         volume = sp.current_playback()['device']['volume_percent']
@@ -48,32 +41,22 @@ def reproducir_cancion(cancion, device='COMPUTADORA'):
             cp = sp.currently_playing()
             cp = cp['item']['uri']
             cola.append(cp)
-        cola.pop(len(cola)-1)
+
+        cola.pop(len(cola) - 1)
         sp.volume(volume)
         for track in cola:
             sp.add_to_queue(track)
         return True
     except SpotifyException:
-        try:
-            track = sp.search(q=f'track:{cancion}', type='track')
-            track = track['tracks']['items'][0]['uri']
-            sp.add_to_queue(track, device_id=config(device))
-            sp.next_track()
-            return True
-        except SpotifyException:
-            return False
+        return False
 
 
-def reanudar_playback(device='COMPUTADORA'):
+def reanudar_playback():
     try:
         sp.start_playback()
         return True
     except SpotifyException:
-        try:
-            sp.start_playback(config(device))
-            return True
-        except SpotifyException:
-            return False
+        return False
 
 
 def siguiente_cancion():
@@ -98,41 +81,39 @@ def dispositivos():
 def get_token():
     print(sa.get_access_token())
 
-def shuffle(device=None):
+
+def get_shuffle_state():
     x = sp.current_playback()
-    x = x['shuffle_state']
-    if device:
-        sp.shuffle(state=not x, device_id=config(device))
-    else:
-        sp.shuffle(state=not x,)
+    return x['shuffle_state']
 
 
+def shuffle(state):
+    sp.shuffle(state=state)
 
-def repetir(device=None):
+
+def repetir():
     x = sp.current_playback()
     if x:
         x = x['repeat_state']
         index = states.index(x)
         index = index + 1 if index != 2 else 0
-        if device:
-            sp.repeat(state=states[index], device_id=config(device))
-        else:
-            sp.repeat(state=states[index],)
+        sp.repeat(state=states[index])
 
 
 def cambiar_dispositivo(device):
     sp.transfer_playback(device_id=config(device), force_play=True)
 
 
-def queue(cancion, device='COMPUTADORA'):
+def agregar_en_cola(cancion):
     track = sp.search(q=f'track:{cancion}', type='track')
     time.sleep(.2)
     track = track['tracks']['items'][0]['uri']
-    sp.add_to_queue(track, device_id=config(device))
+    sp.add_to_queue(track)
 
 
 def regresar_cancion():
     sp.previous_track()
+
 
 def cambiar_volumen(valor):
     volume = sp.current_playback()['device']['volume_percent']
@@ -140,3 +121,67 @@ def cambiar_volumen(valor):
     sp.volume(volume)
 
 
+def reproducir_playlist(playlist):
+    playlists = sp.current_user_playlists()
+    playlists = playlists['items']
+    playlists = [pl for pl in playlists if pl['name'] == playlist]
+    if len(playlists) == 0:
+        return -1
+    sp.start_playback(context_uri=playlists[0]['uri'])
+    return 1
+
+
+def agregar_cancion_pl(cancion, playlist):
+    playlists = sp.current_user_playlists()
+    playlists = playlists['items']
+    playlists = [pl for pl in playlists if pl['name'] == playlist]
+    if len(playlists) == 0:
+        return -1
+    pl_id = playlists[0]['id']
+
+    track = cancion
+    if not re.match(regexsuri, cancion):
+        track = sp.search(q=f'track:{cancion}', type='track')
+        track = track['tracks']['items'][0]['uri']
+
+    sp.playlist_add_items(playlist_id=pl_id, items=[track])
+    return 1
+
+
+def borrar_cancion(cancion, playlist):
+    playlists = sp.current_user_playlists()
+    playlists = playlists['items']
+    playlists = [pl for pl in playlists if pl['name'] == playlist]
+    if len(playlists) == 0:
+        return -1
+    pl_id = playlists[0]['id']
+
+    if re.match(regexsuri, cancion):
+        uri = cancion
+    else:
+        tracks = sp.playlist_items(playlist_id=pl_id)
+        tracks = tracks['items']
+        track = [t for t in tracks if t['track']['name'] == cancion]
+        if len(track) == 0:
+            return -2
+        track = track[0]['track']
+        uri = track['uri']
+    body = {
+        "tracks": [
+            {
+                "uri": uri
+            }
+        ]
+    }
+
+    token = sa.get_cached_token()['access_token']
+    headers = {"Accept": "application/json", "Authorization": f"Bearer {token}"}
+    request = requests.delete(f'https://api.spotify.com/v1/playlists/{pl_id}/tracks', headers=headers, json=body)
+    response = request.json()
+    print(response)
+    return 1
+
+
+def crear_playlist(nombre):
+    user = sp.current_user()
+    sp.user_playlist_create(user=user['id'], name=nombre)
