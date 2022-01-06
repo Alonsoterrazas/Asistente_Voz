4eec6d5a5b091b3db8bc88665f9a150efe98b168
Ivan Sanchez <ivanhumberto_16@hotmail.com>
2022-01-05 19:25:52 -0700

    Revert "metamorfosis a poo del bicho"
    This reverts commit 6b856700aba75656bb1a49a81c8a9a11101dbf65.

--- Controllers/SpotifyController.py
+++ Controllers/SpotifyController.py
@@ -2,167 +2,163 @@ import re
 from Models.spotify import siguiente_cancion, pausar_playback, dispositivos, get_token, reproducir_cancion, \
     regresar_cancion, shuffle, get_shuffle_state, reproducir_playlist, agregar_cancion_pl, crear_playlist, reanudar_playback, \
     agregar_en_cola, cambiar_dispositivo, cambiar_volumen, borrar_cancion, cancion_actual, playlist_actual
+from bicho import voz
+
+reRepro = r'\b(?:reproduce)\s[A-Za-z0-9\s]+\s(?:en spotify)'
+reAggcola = r'\b(?:agrega|mete|pon|añade)\s[A-Za-z0-9\s]+\s(?:en la cola|a la cola)'
+reCambdisp = r'\b(?:reproduce spotify en)\s(?:el|la)\s[A-Za-z0-9\s]+'
+reCambdisp2 = r'\b(?:pasa el spotify)\s(?:al|a la)\s[A-Za-z0-9\s]+'
+reReproPl = r'\b(?:reproduce|pon)\s(?:la playlist)\s[A-Za-z0-9\s]+\s(?:en spotify)'
+reAggCPl = r'\b(?:agrega|mete|pon|añade)\s[A-Za-z0-9\s]+\s(?:a|en)\s(?:la playlist)\s[A-Za-z0-9\s]+'
+reCreaPl = r'\b(?:crea|haz)\s(?:una playlist llamada)\s[A-Za-z0-9\s]+'
+reAggCPL2 = r'\b(?:agrega|mete|pon|añade)\s(?:esta canción)\s(?:a|en)\s(?:la playlist)\s[A-Za-z0-9\s]+'
 
 
-class Spotify_controller:
-    def __init__(self, bicho):
-        self.bicho = bicho
-        self.reRepro = r'\b(?:reproduce)\s[A-Za-z0-9\s]+\s(?:en spotify)'
-        self.reAggcola = r'\b(?:agrega|mete|pon|añade)\s[A-Za-z0-9\s]+\s(?:en la cola|a la cola)'
-        self.reCambdisp = r'\b(?:reproduce spotify en)\s(?:el|la)\s[A-Za-z0-9\s]+'
-        self.reCambdisp2 = r'\b(?:pasa el spotify)\s(?:al|a la)\s[A-Za-z0-9\s]+'
-        self.reReproPl = r'\b(?:reproduce|pon)\s(?:la playlist)\s[A-Za-z0-9\s]+\s(?:en spotify)'
-        self.reAggCPl = r'\b(?:agrega|mete|pon|añade)\s[A-Za-z0-9\s]+\s(?:a|en)\s(?:la playlist)\s[A-Za-z0-9\s]+'
-        self.reCreaPl = r'\b(?:crea|haz)\s(?:una playlist llamada)\s[A-Za-z0-9\s]+'
-        self.reAggCPL2 = r'\b(?:agrega|mete|pon|añade)\s(?:esta canción)\s(?:a|en)\s(?:la playlist)\s[A-Za-z0-9\s]+'
-
-
-    def spotify_main(self, q):
-        if re.match(self.reReproPl, q):
-            tokens = q.split(' ')
-            pl = ' '.join(tokens[3: len(tokens) - 2])
-            band = reproducir_playlist(pl)
-            if band == -1:
-                self.bicho.voz('No se encontró ninguna playlist con ese nombre en tu biblioteca')
-                return
-            self.bicho.voz(f'reproduciendo la playlist {pl} en spotify')
+def spotify_main(q):
+    if re.match(reReproPl, q):
+        tokens = q.split(' ')
+        pl = ' '.join(tokens[3: len(tokens) - 2])
+        band = reproducir_playlist(pl)
+        if band == -1:
+            voz('No se encontró ninguna playlist con ese nombre en tu biblioteca')
             return
-        # Reproducir una canción
-        if re.match(self.reRepro, q):
-            q = q[10:-11]
-            self.bicho.voz(f'reproduciendo {q} en spotify, espere un momento')
-            if not reproducir_cancion(q):
-                self.bicho.voz('Ocurrió un error al reanudar la canción')
-            return
+        voz(f'reproduciendo la playlist {pl} en spotify')
+        return
+    # Reproducir una canción
+    if re.match(reRepro, q):
+        q = q[10:-11]
+        voz(f'reproduciendo {q} en spotify, espere un momento')
+        if not reproducir_cancion(q):
+            voz('Ocurrió un error al reanudar la canción')
+        return
 
-        if q == 'salta la canción' or q == 'salta esa canción' or q == 'siguiente canción' or q == 'quita esa madre':
-            self.bicho.voz('cambiando de canción')
-            siguiente_cancion()
-            return
+    if q == 'salta la canción' or q == 'salta esa canción' or q == 'siguiente canción' or q == 'quita esa madre':
+        voz('cambiando de canción')
+        siguiente_cancion()
+        return
 
-        if q == 'regresa la canción' or q == 'pon la canción anterior':
-            self.bicho.voz('regresando la canción')
-            regresar_cancion()
-            return
+    if q == 'regresa la canción' or q == 'pon la canción anterior':
+        voz('regresando la canción')
+        regresar_cancion()
+        return
 
-        if q == 'pon modo aleatorio' or q == 'activa el modo aleatorio':
-            state = get_shuffle_state()
-            if state:
-                self.bicho.voz('Ya esta activo el modo aleatorio')
-                return
-            shuffle(True)
-            self.bicho.voz('Modo aleatorio activado')
+    if q == 'pon modo aleatorio' or q == 'activa el modo aleatorio':
+        state = get_shuffle_state()
+        if state:
+            voz('Ya esta activo el modo aleatorio')
             return
+        shuffle(True)
+        voz('Modo aleatorio activado')
+        return
 
-        if q == 'desactiva el modo aleatorio' or q == 'quita el modo aleatorio':
-            state = get_shuffle_state()
-            if not state:
-                self.bicho.voz('Ya esta desactivo el modo aleatorio')
-                return
-            shuffle(False)
-            self.bicho.voz('Modo aleatorio desactivado')
+    if q == 'desactiva el modo aleatorio' or q == 'quita el modo aleatorio':
+        state = get_shuffle_state()
+        if not state:
+            voz('Ya esta desactivo el modo aleatorio')
             return
+        shuffle(False)
+        voz('Modo aleatorio desactivado')
+        return
 
-        if q == 'pausa la canción' or q == 'ponle pausa' or q == 'pon pausa':
-            self.bicho.voz('pausando la cancion')
-            pausar_playback()
-            return
+    if q == 'pausa la canción' or q == 'ponle pausa' or q == 'pon pausa':
+        voz('pausando la cancion')
+        pausar_playback()
+        return
 
-        if q == 'ponle play' or q == 'reanuda la canción':
-            self.bicho.voz('reanudando la canción')
-            reanudar_playback()
-            return
+    if q == 'ponle play' or q == 'reanuda la canción':
+        voz('reanudando la canción')
+        reanudar_playback()
+        return
 
-        if q == 'sube el volumen' or q == 'súbele al volumen':
-            self.bicho.voz('subiendo el volumen')
-            cambiar_volumen(10)
-            return
+    if q == 'sube el volumen' or q == 'súbele al volumen':
+        voz('subiendo el volumen')
+        cambiar_volumen(10)
+        return
 
-        if q == 'baja el volumen' or q == 'bájale al volumen':
-            self.bicho.voz('bajando el volumen')
-            cambiar_volumen(-10)
-            return
+    if q == 'baja el volumen' or q == 'bájale al volumen':
+        voz('bajando el volumen')
+        cambiar_volumen(-10)
+        return
 
-        if re.match(self.reAggcola, q):
-            tokens = q.split(' ')
-            song = ' '.join(tokens[1: len(tokens)-3])
-            self.bicho.voz(f'agregando {song} a la cola')
-            agregar_en_cola(song)
-            return
+    if re.match(reAggcola, q):
+        tokens = q.split(' ')
+        song = ' '.join(tokens[1: len(tokens)-3])
+        voz(f'agregando {song} a la cola')
+        agregar_en_cola(song)
+        return
 
-        if re.match(self.reCambdisp, q):
-            disp = q[24:]
-            disp = disp.upper()
-            self.bicho.voz(f'cambiando dispositivo a {disp}')
-            cambiar_dispositivo(disp)
-            return
-        if re.match(self.reCambdisp2, q):
-            tokens = q.split(' ')
-            if tokens[3] == 'al':
-                index = 4
-            else:
-                index = 5
-            disp = ' '.join(tokens[index: len(tokens)])
-            disp = disp.upper()
-            self.bicho.voz(f'cambiando dispositivo a {disp}')
-            cambiar_dispositivo(disp)
-            return
+    if re.match(reCambdisp, q):
+        disp = q[24:]
+        disp = disp.upper()
+        voz(f'cambiando dispositivo a {disp}')
+        cambiar_dispositivo(disp)
+        return
+    if re.match(reCambdisp2, q):
+        tokens = q.split(' ')
+        if tokens[3] == 'al':
+            index = 4
+        else:
+            index = 5
+        disp = ' '.join(tokens[index: len(tokens)])
+        disp = disp.upper()
+        voz(f'cambiando dispositivo a {disp}')
+        cambiar_dispositivo(disp)
+        return
 
-        if re.match(self.reAggCPl, q):
-            tokens = q.split(' ')
-            pl_i = tokens.index('playlist')
-            song = ' '.join(tokens[1: pl_i-2])
-            pl = ' '.join(tokens[pl_i+1:])
-            band = agregar_cancion_pl(song, pl)
-            if band == -1:
-                self.bicho.voz('No se encontró ninguna playlist con ese nombre en tu biblioteca')
-                return
-            self.bicho.voz(f'{song} agregado a la playlist {pl}')
+    if re.match(reAggCPl, q):
+        tokens = q.split(' ')
+        pl_i = tokens.index('playlist')
+        song = ' '.join(tokens[1: pl_i-2])
+        pl = ' '.join(tokens[pl_i+1:])
+        band = agregar_cancion_pl(song, pl)
+        if band == -1:
+            voz('No se encontró ninguna playlist con ese nombre en tu biblioteca')
             return
+        voz(f'{song} agregado a la playlist {pl}')
+        return
 
-        if re.match(self.reCreaPl, q):
-            tokens = q.split(' ')
-            pl = ' '.join(tokens[4])
-            self.bicho.voz(f'creando la playlist {pl}')
-            crear_playlist(pl)
-            return
+    if re.match(reCreaPl, q):
+        tokens = q.split(' ')
+        pl = ' '.join(tokens[4])
+        voz(f'creando la playlist {pl}')
+        crear_playlist(pl)
+        return
 
-        if re.match(self.reAggCPL2, q):
-            song = cancion_actual()
-            tokens = q.split(' ')
-            pl_i = tokens.index('playlist')
-            pl = ' '.join(tokens[pl_i + 1:])
-            band = agregar_cancion_pl(song, pl)
-            if band == -1:
-                self.bicho.voz('No se encontró ninguna playlist con ese nombre en tu biblioteca')
-                return
-            self.bicho.voz(f'Se ha agregado a la playlist {pl}')
+    if re.match(reAggCPL2, q):
+        song = cancion_actual()
+        tokens = q.split(' ')
+        pl_i = tokens.index('playlist')
+        pl = ' '.join(tokens[pl_i + 1:])
+        band = agregar_cancion_pl(song, pl)
+        if band == -1:
+            voz('No se encontró ninguna playlist con ese nombre en tu biblioteca')
             return
+        voz(f'Se ha agregado a la playlist {pl}')
+        return
 
-        if q == 'quita esta canción de la playlist' or q == 'saca esta canción de la playlist' or q == 'saca esta madre de la playlist':
-            pl = playlist_actual()
-            if pl == -1:
-                self.bicho.voz('No estas escuchando ninguna playlist')
-                return
-            if pl == -2:
-                self.bicho.voz('No puedes quitar canciones en esta playlist')
-                return
-            song = cancion_actual()
-            band = borrar_cancion(song, pl)
-            if band == -1:
-                self.bicho.voz('No se encontró ninguna playlist con ese nombre en tu biblioteca')
-                return
-            if band == -2:
-                self.bicho.voz('No se encontró esta canción dentro de la playlist')
-                return
-            self.spotify.siguiente_cancion()
-            self.bicho.voz(f'Se ha eliminado a la playlist {pl}')
+    if q == 'quita esta canción de la playlist' or q == 'saca esta canción de la playlist' or q == 'saca esta madre de la playlist':
+        pl = playlist_actual()
+        if pl == -1:
+            voz('No estas escuchando ninguna playlist')
             return
+        if pl == -2:
+            voz('No puedes quitar canciones en esta playlist')
+            return
+        song = cancion_actual()
+        band = borrar_cancion(song, pl)
+        if band == -1:
+            voz('No se encontró ninguna playlist con ese nombre en tu biblioteca')
+            return
+        if band == -2:
+            voz('No se encontró esta canción dentro de la playlist')
+            return
+        voz(f'Se ha eliminado a la playlist {pl}')
+        return
 
-        if 'equipos' in q:
-            dispositivos()
-            return
+    if 'equipos' in q:
+        dispositivos()
+        return
 
-        if 'token' in q:
-            get_token()
-            return
+    if 'token' in q:
+        get_token()
+        return
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 from requests.exceptions import ConnectTimeout, ReadTimeout
-from bicho import Bicho
+from bicho import listen_action, voz, call
 from Models.games import piedra_papel_tijeras
 from Controllers.SpotifyController import spotify_main
 import pywhatkit
@@ -10,107 +10,75 @@ from Models.player import reproducir_arch
 import requests
 from Views.Configuracion import VentanaConfiguracion
 
-class Main:
-    def __init__(self):
-        self.regexsyt = r'\b(?:reproduce)\s[a-z0-9\s]+\s(?:en)\s(?:youtube)'
-        self.bicho = Bicho()
-        self.regexsruta = r'\b(?:abre)\s[a-z0-9\s]'
+regexsyt = r'\b(?:reproduce)\s[a-z0-9\s]+\s(?:en)\s(?:youtube)'
 
 
-    def querying(self):
-        start = True
-        while start:
-            q = self.bicho.listen_action()
-            if not q:
-                continue
-            q = q.lower()
-            print(q)
+def querying():
+    start = True
+    while start:
+        q = listen_action()
+        if not q:
+            continue
+        q = q.lower()
+        print(q)
 
-            if re.match(self.regexsyt, q):
-                index = q.find('en')
-                q = q[10:index]
-                self.bicho.voz(f'reproduciendo {q} en youtube.')
-                pywhatkit.playonyt(q)
-                break
+        if re.match(regexsyt, q):
+            index = q.find('en')
+            q = q[10:index]
+            voz(f'reproduciendo {q} en youtube.')
+            pywhatkit.playonyt(q)
+            continue
 
-            if 'busca' in q:
-                q = q[6:]
-                self.bicho.voz(f'Buscando {q}, espera un momento ')
-                pywhatkit.search(q)
-                break
+        if 'busca' in q:
+            q = q[6:]
+            voz(f'Buscando {q}, espera un momento ')
+            pywhatkit.search(q)
+            continue
 
-            if 'abre la configuración' in q:
-                self.bicho.voz(f'abriendo la ventana de configuración')
-                vc = VentanaConfiguracion()
-                vc.mostrarVista()
-                break
-
-            if re.match(self.regexsruta, q):
-                index = q.find('e')
-                nombre = q[index + 2:]
-                self.bicho.voz(f'abriendo {nombre}')
-
-                with open('data.txt', 'r') as file:
-                    filedata = file.read()
-                    indexnombre = filedata.index(nombre)
-                    indexruta1 = filedata.index("'", indexnombre) + 1
-                    indexruta2 = filedata.index("'", indexruta1)
-                    ruta = filedata[indexruta1:indexruta2]
-                os.system(f'"{ruta}"')
-                break
-
-
-
-            if 'alimenta al perro' in q:
-                url = 'http://192.168.3.18/feedbuttonclick'
-                try:
-                    requests.get(url, timeout=2)
-                except ConnectTimeout:
-                    self.bicho.voz('El alimentador se encuentra fuera de conexion')
-                    reproducir_arch('Siu')
-                except ReadTimeout:
-                    self.bicho.voz('se ha servido la comida')
-                    reproducir_arch('Siu')
-                break
-
-            if self.spotify_command(q):
-                spotify_main(q)
-                continue
-
-            if 'piedra papel o tijeras' in q:
-                self.bicho.voz('preparate para el duelo')
-                piedra_papel_tijeras()
-                continue
-
-            if q == 'adiós':
-                self.bicho.voz('tendré que dejar de luchar siuuuu')
-                # TODO meter audio de fue muy bonito estar en madrid
-                break
-
-    def spotify_command(self, q):
-        return 'spotify' or 'volumen' or 'modo aleatorio' or 'canción' or 'playlist' or 'la cola' \
-               or 'pausa' or 'ponle play' or 'quita esa madre' in q
-
-
-    # función que espera a que digas la palabra magic
-    def wait_for_call(self):
-        while True:
-            v = self.bicho.call()
-            v = v.lower() if v else None
-            print(v)
-            with open('data.txt', 'r') as file:
-                filedata = file.read()
-                indexcmdact = filedata.index('COMANDO_ACTIVACION')
-                indexcmdviejo1 = filedata.index("'", indexcmdact) + 1
-                indexcmdviejo2 = filedata.index("'", indexcmdviejo1)
-                comact = filedata[indexcmdviejo1:indexcmdviejo2]
-            if v and comact in v:
+        if 'alimenta al perro' in q:
+            url = 'http://192.168.3.18/feedbuttonclick'
+            try:
+                requests.get(url, timeout=2)
+            except ConnectTimeout:
+                voz('El alimentador se encuentra fuera de conexion')
                 reproducir_arch('Siu')
-                self.querying()
+            except ReadTimeout:
+                voz('se ha servido la comida')
+                reproducir_arch('Siu')
+            continue
+
+        if spotify_command(q):
+            spotify_main(q)
+            continue
+
+        if 'piedra papel o tijeras' in q:
+            voz('preparate para el duelo')
+            piedra_papel_tijeras()
+            continue
+
+        if q == 'adiós':
+            voz('tendré que dejar de luchar siuuuu')
+            # TODO meter audio de fue muy bonito estar en madrid
+            break
+
+
+def spotify_command(q):
+    return 'spotify' or 'volumen' or 'modo aleatorio' or 'canción' or 'playlist' or 'la cola'\
+           or 'pausa' or 'ponle play' or 'quita esa madre' in q
+
+
+# función que espera a que digas la palabra magic
+def wait_for_call():
+    while True:
+        v = call()
+        v = v.lower() if v else None
+        print(v)
+        if v and 'okay bicho' in v:
+            reproducir_arch('Siu')
+            querying()
 
 
 if __name__ == '__main__':
-    main = Main()
-    main.wait_for_call()
+    wait_for_call()
 
 
